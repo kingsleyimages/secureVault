1084f2f25d0fc48b01de0a0dddc8dbb80d2e4d84
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2024-12-11 11:52:01 -0700

    fix combination

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ function vault() {
   let message =
     'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
   // create three variables and assign calculations
-  const num1 = 2025 / 75;
-  const num2 = 2024 - 2009;
-  const num3 = 2 * 4 + 6;
+  const num1 = 2000 / 200;
+  const num2 = 2024 - 1984;
+  const num3 = 2 * 4 * 6 + 1;
   // output alert that contains the message and combination to the vault
   alert(message + '\n\n' + num1 + ',' + num2 + ',' + num3);
   console.log(message + '\n\n' + num1 + ',' + num2 + ',' + num3);
