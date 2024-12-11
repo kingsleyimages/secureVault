9c67bca99b8d14520fd43af18aee0f64cd8906cd
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2024-12-11 11:53:32 -0700

    fix combination

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function vault() {
   // create three variables and assign calculations
   const num1 = 2000 / 200;
   const num2 = 2024 - 1984;
-  const num3 = 2 * 4 * 6 + 1;
+  const num3 = 2 * 4 * 6 - 9;
   // output alert that contains the message and combination to the vault
   alert(message + '\n\n' + num1 + ',' + num2 + ',' + num3);
   console.log(message + '\n\n' + num1 + ',' + num2 + ',' + num3);
